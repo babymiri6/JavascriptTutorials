6ad0c81c47dbe5f8851abb40e6dd5c51b78b6616
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-10 23:48:16 +0000

    I learnt how and why to use const to declare variables

--- Variables/variableConst.js
+++ Variables/variableConst.js
@@ -1,1 +1,20 @@
 // Const means a variable that cannot change
+// it adds some security to our code
+
+//Simple formula to calculate the circumference of a circle
+const PI = 3.14159
+let radius;
+let circumference;
+
+// HOW TO ACCEPT USER INPUT
+radius = window.prompt("Enter the radius of the circle");
+
+//CONVERT THE USER INPUT INTO A STRING CAUSE IT IS ORIGNALLY A NUMBER
+radius = Number(radius);
+
+//PI = 420.69;
+
+//CALCULATING CIRCUMFERENCE
+circumference = 2 * PI * radius;
+
+console.log(`The circumference is ${circumference}`);
