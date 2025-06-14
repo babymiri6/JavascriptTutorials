68c6e14f524003f19ff389e299ffbc4d13d0c5d2
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-14 23:11:41 +0000

    I did another example today haha

--- Variables/ifStatements.js
+++ Variables/ifStatements.js
@@ -1,11 +1,21 @@
 // IF STATEMENTS = if a condition is true, execute some code
 //                  if not. do something else
 
-let age = 25;
+// let age = 25;
 
-if(age >= 18){
-    console.log("You are old enough to enter this site");
+// if(age >= 18){
+//     console.log("You are old enough to enter this site");
+// }
+// else{
+//     console.log("You must be 18 to enter this site");
+// }
+
+//Example 2
+let time = 9;
+
+if(time < 12){
+    console.log("Good morning");
 }
 else{
-    console.log("You must be 18 to enter this site");
+    console.log("Good afternoon");
 }
