c9e62e870e836c4fb191568c758c64c7dcfe32b8
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-15 22:17:44 +0000

    I did two  more examples on if statements, with one involving a boolean to finalise my understanding on IF STATEMENT

--- Variables/ifStatements.js
+++ Variables/ifStatements.js
@@ -11,11 +11,39 @@
 // }
 
 //Example 2
-let time = 9;
+// let time = 9;
 
-if(time < 12){
-    console.log("Good morning");
+// if(time < 12){
+//     console.log("Good morning");
+// }
+// else{
+//     console.log("Good afternoon");
+// }
+
+//Example 3
+// let isStudent = false;
+
+// if(isStudent){
+//     console.log("You are a student");
+// }
+// else{
+//     console.log("You are not a student")
+// }
+
+//Example 4 - Adding an IF statement within anpther IF statement
+let age = 17;
+let hasLicense = false;
+
+if(age >= 18){
+    console.log("You are old enough to drive");
+
+    if(hasLicense){
+        console.log("You have your license!");
+    }
+    else{
+        console.log("Yolu do not have your license yet!")
+    }
 }
 else{
-    console.log("Good afternoon");
+    console.log("You must be 18+ to have a license");
 }
