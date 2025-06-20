eafb2c7a83b80f210970c799e4c2bf4eb40de516
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-20 23:11:32 +0000

    I converted the else if statement to a switch statement

--- Tutorials/switchStatements.js
+++ Tutorials/switchStatements.js
@@ -4,20 +4,60 @@
 //WRITING AN ELSE IF STATEMENT then writing the same
 // logic with a SWITCH STATEMENT
 
-let day = 7;
+//ELSE IF FORMAT
+// let day = 8;
 
-if(day = 1){
-    console.log("Monday");
-}
-else if (day = 2){
-    console.log("Tuesday");
-}
-else if (day = 3){
-    console.log("Wednesday");
-}
-else if (day = 4){
-    console.log("Thursday");
-}
-else if (day = 5){
-    console.log("Friday");
+// if(day == 1){
+//     console.log("Monday");
+// }
+// else if(day == 2){
+//     console.log("Tuesday");
+// }
+// else if(day == 3){
+//     console.log("Wednesday");
+// }
+// else if(day == 4){
+//     console.log("Thursday");
+// }
+// else if(day == 5){
+//     console.log("Friday");
+// }
+// else if(day == 6){
+//     console.log("Saturday");
+// }
+// else if(day == 7){
+//     console.log("Sunday");
+// }
+// else{
+//     console.log(`${day} is not a day`);
+// }
+
+//SWITCH FORMAT
+let day = "Pizza"
+
+switch(day){
+    case 1:
+        console.log("It is Monday");
+        break;      //Always remember to add break in switches
+     case 2:
+        console.log("It is Tuesday");
+        break;      //We add the break statements so that
+     case 3:        //we can break out the switch once we have a match
+        console.log("It is Wednesday");
+        break;
+     case 4:
+        console.log("It is Thursday");
+        break;
+     case 5:
+        console.log("It is Friday");
+        break;
+     case 6:
+        console.log("It is Saturday");
+        break;
+     case 7:
+        console.log("It is Sunday");
+        break;
+    //You can add a default case in case there are no matches
+    default:
+        console.log(`${day} is not a day`);
 }
