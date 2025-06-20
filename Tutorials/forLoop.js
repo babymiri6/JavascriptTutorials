5e3bb3075a452abe978fa38f89c2d34cfdff6d33
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-20 23:41:12 +0000

    Simple for loop examples

---
+++ Tutorials/forLoop.js
@@ -0,0 +1,15 @@
+// FOR LOOP - Repeat some code a LIMITED amount of times
+
+// for(let i = 0; i <= 2; i++){  //we created a temporary counter, then a condition, then an increment in the parenthesis
+//     console.log("Hello");
+// }
+
+// for(let i = 2; i <= 10; i+=2){  //increment in twos
+//      console.log(i);
+// }
+
+for(let i = 10; i > 0; i--){  //decrement in ones
+     console.log(i);
+}
+
+console.log("HAPPY NEW YEAR")
