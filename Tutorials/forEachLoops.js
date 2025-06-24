4c2448cd4f4089d8206e8648768fe714a454c265
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-24 23:39:23 +0000

    First example on forEach loops

---
+++ Tutorials/forEachLoops.js
@@ -0,0 +1,15 @@
+// forEach() = method used to iterate over the elements
+//      of an array and apply a specfied function (callback)
+//      to each element
+
+//      array.forEach(callback)
+//      element, index, array are provided
+
+let numbers = [1, 2, 3, 4 , 5];
+
+numbers.forEach(display);
+function display(element){
+    console.log(element);
+}
+
+
