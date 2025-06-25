8d1b094fcc0ce23dbeb074cddd40d5115823f3e7
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-25 00:37:21 +0000

    Tried more examples using the FOREACH loop

--- Tutorials/forEachLoops.js
+++ Tutorials/forEachLoops.js
@@ -7,7 +7,29 @@
 
 let numbers = [1, 2, 3, 4 , 5];
 
+numbers.forEach(cube);
+// numbers.forEach(double);
 numbers.forEach(display);
+
+function double(element, index, array){
+    array[index] = element * 2;
+}
+
+function triple(element, index, array){
+    array[index] = element * 3;
+}
+// function square(element, index, array){
+//     array[index] = element * element;
+// }
+
+//OR
+function square(element, index, array){
+    array[index] = Math.pow(element, 2);
+}
+
+function cube(element, index, array){
+    array[index] = Math.pow(element, 3);
+}
 function display(element){
     console.log(element);
 }
