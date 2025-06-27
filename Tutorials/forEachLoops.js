811a5f5800b41246a3a3c0d09ec7d9d21b899cb7
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-27 01:19:06 +0000

    I tried more commplec examples using the forEach loop. That's a wrap on this loop. On to for of

--- Tutorials/forEachLoops.js
+++ Tutorials/forEachLoops.js
@@ -5,33 +5,68 @@
 //      array.forEach(callback)
 //      element, index, array are provided
 
-let numbers = [1, 2, 3, 4 , 5];
+// let numbers = [1, 2, 3, 4 , 5];
 
-numbers.forEach(cube);
-// numbers.forEach(double);
-numbers.forEach(display);
+// numbers.forEach(cube);
+// // numbers.forEach(double);
+// numbers.forEach(display);
 
-function double(element, index, array){
-    array[index] = element * 2;
-}
-
-function triple(element, index, array){
-    array[index] = element * 3;
-}
-// function square(element, index, array){
-//     array[index] = element * element;
+// function double(element, index, array){
+//     array[index] = element * 2;
 // }
 
-//OR
-function square(element, index, array){
-    array[index] = Math.pow(element, 2);
+// function triple(element, index, array){
+//     array[index] = element * 3;
+// }
+// // function square(element, index, array){
+// //     array[index] = element * element;
+// // }
+
+// //OR
+// function square(element, index, array){
+//     array[index] = Math.pow(element, 2);
+// }
+
+// function cube(element, index, array){
+//     array[index] = Math.pow(element, 3);
+// }
+// function display(element){
+//     console.log(element);
+// }
+
+//Second example
+// let fruits = ["apple", "orange", "banana", "coconut"];
+
+// fruits.forEach(uppercase);      //making all the strings of all the elements uppercase before displaying.
+
+// let fruits = ["APPLE", "ORANGE", "BANANA", "COCONUT"];
+
+// fruits.forEach(lowercase);
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+fruits.forEach(capitalize);
+fruits.forEach(display);    //calling the display function.
+
+//Creating a function to make all the strings uppercase
+function uppercase(element, index, array){
+    array[index] = element.toUpperCase();
 }
 
-function cube(element, index, array){
-    array[index] = Math.pow(element, 3);
+//creating a function to make all strings lowercase
+function lowercase(element, index, array){
+    array[index] = element.toLowerCase();
 }
-function display(element){
+
+// creating a function to capitalize all strings
+function capitalize(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+//"charAt()" is used to get the element at the index specified
+// in the "()"
+//"slice(1)" creates a substring of every element beginning at index 1.
+
+function display(element){      //creating the display function
     console.log(element);
 }
-
 
