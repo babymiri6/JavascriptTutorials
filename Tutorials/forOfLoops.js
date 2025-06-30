5018e44c91f8e764ba8c429d15aef11ef230df6c
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-06-30 23:11:41 +0000

    I completed a simple task using the for of loop

--- Tutorials/forOfLoops.js
+++ Tutorials/forOfLoops.js
@@ -15,16 +15,34 @@
 //     console.log(letter);
 // }
 
-let characters = ['Sonic', 'Tails', 'Mario', 'Luigi', 'Link'];
+// let characters = ['Sonic', 'Tails', 'Mario', 'Luigi', 'Link'];
 
 //Similar to "for in" loop except for in prints indexes instead of chars
+//"for in" is for looping over properties of an object
 //syntax "for variable in iterable"
-for (i of characters){
-    if(i === "Luigi"){
+// for (i of characters){
+//     if(i === "Luigi"){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+//Challenge 1
+let colours = ["Red", "Orange", "Yellow", "Green", "Blue"];
+
+// for (colour of colours){
+//     console.log(colour);
+// }
+
+//challenge 2
+for (colour of colours){
+    if(colour === "Green"){
         break;
     }
     else{
-        console.log(i);
+        console.log(colour);
     }
 }
 
